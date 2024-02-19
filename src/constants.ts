b0a8f29e87a362dc56d4500fc03add8aa8c1fa8b
IndieUnityMaker <ubivarik_serega@mail.ru>
2024-02-20 00:29:10 +0300

    Добавил константы городов.

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+const LOCATIONS = {
+  Paris: 'Paris',
+  Cologne: 'Cologne',
+  Brussels: 'Brussels',
+  Amsterdam: 'Amsterdam',
+  Hamburg: 'Hamburg',
+  Dusseldorf: 'Dusseldorf',
+};
+
+
+export { LOCATIONS };
