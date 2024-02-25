c0977b80e78ab9a451666e572c829d582c11f045
IndieUnityMaker <ubivarik_serega@mail.ru>
2024-02-25 23:59:34 +0300

    обернул в PrivateRoute

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -7,7 +7,8 @@ import OfferPage from '../pages/offer-page';
 import ErrorPage from '../pages/error-page';
 import { AppRoute } from '../constants';
 import { cardsData } from '../mock/card-data';
-import { AuthorizationStatus } from '../constants';
+import Layout from './layout';
+import { getAuthorizationStatus } from '../mock/auth-status';
 
 export default function App (): JSX.Element {
 
@@ -15,15 +16,25 @@ export default function App (): JSX.Element {
   return(
     <BrowserRouter>
       <Routes>
-        <Route path={AppRoute.Main} element = {<MainPage data={ cardsData }/>}/>
-        <Route path={AppRoute.Login} element = {<LoginPage/>}/>
-        <Route path={AppRoute.Favorites} element = {
-          <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
-            <FavoritesPage/>
-          </PrivateRoute>
-        }
-        />
-        <Route path={AppRoute.Offer} element = {<OfferPage/>}/>
+        <Route
+          path={AppRoute.Main}
+          element = {<Layout />}
+        >
+          <Route index element = {<MainPage data={ cardsData }/>}/>
+          <Route path={AppRoute.Login} element = {
+            <PrivateRoute authorizationStatus={getAuthorizationStatus()} isReverse>
+              <LoginPage/>
+            </PrivateRoute>
+          }
+          />
+          <Route path={AppRoute.Favorites} element = {
+            <PrivateRoute authorizationStatus={getAuthorizationStatus()}>
+              <FavoritesPage/>
+            </PrivateRoute>
+          }
+          />
+          <Route path={AppRoute.Offer} element = {<OfferPage/>}/>
+        </Route>
         <Route path='*' element = {<ErrorPage/>}/>
       </Routes>
     </BrowserRouter>
