e2020128badc59d821025a1600481c6da1647dac
IndieUnityMaker <ubivarik_serega@mail.ru>
2024-03-09 21:15:59 +0300

    добавлен тип для оффера

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,21 @@
 
+type typeLocation = {
+  latitude: number;
+  longitude: number;
+  zoom: number;
+};
+
+type typeCity = {
+  name: string;
+  location: typeLocation;
+};
+
+type typeHost = {
+  name: string;
+  avatarUrl: string;
+  isPro: boolean;
+}
+
 type typeCard = {
   id: string;
   title: string;
@@ -8,11 +25,30 @@ type typeCard = {
   isFavorite: boolean;
   isPremium: boolean;
   rating: number;
+  city: typeCity;
+  location: typeLocation;
 };
+type typeOffer = {
+  id: string;
+  title: string;
+  type: string;
+  price: number;
+  city: typeCity;
+  location: typeLocation;
+  isFavorite: boolean;
+  isPremium: boolean;
+  rating: number;
+  description: string;
+  bedrooms: number;
+  goods: [string];
+  host: typeHost;
+  images: [string];
+  maxAdults: number;
+  }
 
 type LocationButtonType = {
   name: string;
   isActive: boolean;
 };
 
-export type { LocationButtonType, typeCard };
+export type { LocationButtonType, typeCard, typeOffer };
