a091a9ec0cf8967a45a85b9793e50f0f848b5d38
IndieUnityMaker <ubivarik_serega@mail.ru>
2024-03-31 17:14:23 +0300

    Добавлены функции форматирования даты в формат: March 2024 и 2024-03-31

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,2 +1,15 @@
 
 export const capitalizedWord = (str:string) => str.charAt(0).toUpperCase() + str.slice(1);
+
+export const formatDateNormalize = (dateData: string) => (
+  new Date(dateData).toLocaleDateString('en-US', { year: 'numeric', month: 'long' })
+);
+
+export const formatDateTime = (dateData: string) => {
+  const date = new Date(dateData);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
+};
