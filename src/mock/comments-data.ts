501d838442790502296638d8deeb358f3def2070
IndieUnityMaker <ubivarik_serega@mail.ru>
2024-03-31 17:13:08 +0300

    моки отзывов

---
+++ src/mock/comments-data.ts
@@ -0,0 +1,135 @@
+
+export const commentsData = [
+  {
+    'id': '1dc846bf-d4ac-43ab-8da9-d8c33bdbe9d7',
+    'comment': 'Design interior in most sympathetic area! Complitely renovated, well-equipped, cosy studio in idyllic, over 100 years old wooden house. Calm street, fast connection to center and airport.',
+    'date': '2024-03-31T04:31:50.039Z',
+    'rating': 4,
+    'user': {
+      'name': 'test',
+      'avatarUrl': 'https://15.design.htmlacademy.pro/static/avatar/5.jpg',
+      'isPro': false
+    }
+  },
+  {
+    'id': '1dc846bf-d4ac-43ab-8da9-d8c33bdbe9d7',
+    'comment': 'Fast connection to center and airport. That`s it)',
+    'date': '2024-03-31T04:31:50.039Z',
+    'rating': 2,
+    'user': {
+      'name': 'test',
+      'avatarUrl': 'https://15.design.htmlacademy.pro/static/avatar/5.jpg',
+      'isPro': false
+    }
+  },
+  {
+    'id': '1dc846bf-d4ac-43ab-8da9-d8c33bdbe9d7',
+    'comment': 'over 100 years old wooden house. Calm street, fast connection to center and airport.',
+    'date': '2024-03-31T04:34:34.217Z',
+    'rating': 4,
+    'user': {
+      'name': 'test',
+      'avatarUrl': 'https://15.design.htmlacademy.pro/static/avatar/5.jpg',
+      'isPro': false
+    }
+  },
+  {
+    'id': '9f7c9043-29cd-4128-ac0d-889283f09acf',
+    'comment': 'over 100 years old wooden house. Calm street, fast connection to center and airport.',
+    'date': '2024-03-31T04:34:34.217Z',
+    'rating': 1,
+    'user': {
+      'name': 'test',
+      'avatarUrl': 'https://15.design.htmlacademy.pro/static/avatar/5.jpg',
+      'isPro': false
+    }
+  },
+  {
+    'id': 'b73a8604-ca4d-4659-93ff-3a0874961f74',
+    'comment': 'over 100 years old wooden house. Calm street, fast connection to center and airport.',
+    'date': '2024-03-31T04:34:34.217Z',
+    'rating': 2,
+    'user': {
+      'name': 'test',
+      'avatarUrl': 'https://15.design.htmlacademy.pro/static/avatar/5.jpg',
+      'isPro': false
+    }
+  },
+  {
+    'id': '975b5fcb-59a4-4063-bf63-f59b8e217723',
+    'comment': 'over 100 years old wooden house. Calm street, fast connection to center and airport.',
+    'date': '2024-03-31T04:34:34.217Z',
+    'rating': 2,
+    'user': {
+      'name': 'test',
+      'avatarUrl': 'https://15.design.htmlacademy.pro/static/avatar/5.jpg',
+      'isPro': false
+    }
+  },
+  {
+    'id': '343b71b0-d1e5-4580-bf5c-1f0f3089abb7',
+    'comment': 'over 100 years old wooden house. Calm street, fast connection to center and airport.',
+    'date': '2024-03-31T04:34:34.217Z',
+    'rating': 3,
+    'user': {
+      'name': 'test',
+      'avatarUrl': 'https://15.design.htmlacademy.pro/static/avatar/5.jpg',
+      'isPro': false
+    }
+  },
+  {
+    'id': '1dc846bf-d4ac-43ab-8da9-d8c33bdbe9d7',
+    'comment': 'over 100 years old wooden house. Calm street, fast connection to center and airport.',
+    'date': '2024-03-31T04:34:34.217Z',
+    'rating': 2,
+    'user': {
+      'name': 'test',
+      'avatarUrl': 'https://15.design.htmlacademy.pro/static/avatar/5.jpg',
+      'isPro': false
+    }
+  },
+  {
+    'id': '9f7c9043-29cd-4128-ac0d-889283f09acf',
+    'comment': 'over 100 years old wooden house. Calm street, fast connection to center and airport.',
+    'date': '2024-03-31T04:34:34.217Z',
+    'rating': 5,
+    'user': {
+      'name': 'test',
+      'avatarUrl': 'https://15.design.htmlacademy.pro/static/avatar/5.jpg',
+      'isPro': false
+    }
+  },
+  {
+    'id': 'b73a8604-ca4d-4659-93ff-3a0874961f74',
+    'comment': 'over 100 years old wooden house. Calm street, fast connection to center and airport.',
+    'date': '2024-03-31T04:34:34.217Z',
+    'rating': 5,
+    'user': {
+      'name': 'test',
+      'avatarUrl': 'https://15.design.htmlacademy.pro/static/avatar/5.jpg',
+      'isPro': false
+    }
+  },
+  {
+    'id': '975b5fcb-59a4-4063-bf63-f59b8e217723',
+    'comment': 'over 100 years old wooden house. Calm street, fast connection to center and airport.',
+    'date': '2024-03-31T04:34:34.217Z',
+    'rating': 5,
+    'user': {
+      'name': 'test',
+      'avatarUrl': 'https://15.design.htmlacademy.pro/static/avatar/5.jpg',
+      'isPro': false
+    }
+  },
+  {
+    'id': '343b71b0-d1e5-4580-bf5c-1f0f3089abb7',
+    'comment': 'over 100 years old wooden house. Calm street, fast connection to center and airport.',
+    'date': '2024-03-31T04:34:34.217Z',
+    'rating': 2,
+    'user': {
+      'name': 'test',
+      'avatarUrl': 'https://15.design.htmlacademy.pro/static/avatar/5.jpg',
+      'isPro': false
+    }
+  }
+];
