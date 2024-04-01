b3f82fbacf2f818dee6d9401117acf3caa9d1a7d
IndieUnityMaker <ubivarik_serega@mail.ru>
2024-04-01 17:52:41 +0300

    add sort by city

--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -3,35 +3,38 @@ import CardOffer from '../components/card/card-offer';
 import LocationButton from '../components/location-button';
 import PlacesSortForm from '../components/places-sort-form';
 
-
 import { typeCard } from '../types';
 import { Nullable } from 'vitest';
 import { LOCATIONS } from '../constants';
 import { Map } from '../components/map';
+import { useAppSelector } from '../store/helpers';
 
 export default function MainPage ({data}: {data: typeCard[]}): JSX.Element {
 
   const [activeOffer, setActiveOffer] = useState<Nullable<typeCard>>(null);
+  const currentCity = useAppSelector((state) => state.currentCity);
 
   const handleHoverOnCard = (offer?: typeCard): void => {
     setActiveOffer(offer || null);
   };
-
-  const cardList = data.map((cardItem) => (
+  const filteredDataByCity = data.filter((item) => item.city.name === currentCity);
+  const cardList = filteredDataByCity.map((cardItem) => (
     <CardOffer
       key={cardItem.id}
       card={cardItem}
       handleHover={handleHoverOnCard}
     />
   ));
-    //В результате работы 36 строки образуется визуальный баг - синяя полоса сверху. Очеь интересно почему так
+  const buttonList = Object.keys(LOCATIONS).map((item) => <LocationButton key={item} name={item} isActive = {currentCity === item} />);
+
+  //В результате работы 36 строки образуется визуальный баг - синяя полоса сверху. Очеь интересно почему так
   return(
     <main className="page__main page__main--index">
       <h1 className="visually-hidden">Cities</h1>
       <div className="tabs">
         <section className="locations container">
           <ul className="locations__list tabs__list">
-            {Object.keys(LOCATIONS).map((item) => <LocationButton key={item} name={item} isActive = {false} />)}
+            {buttonList}
           </ul>
         </section>
       </div>
@@ -39,7 +42,7 @@ export default function MainPage ({data}: {data: typeCard[]}): JSX.Element {
         <div className="cities__places-container container">
           <section className="cities__places places">
             <h2 className="visually-hidden">Places</h2>
-            <b className="places__found">312 places to stay in Amsterdam</b>
+            <b className="places__found">{filteredDataByCity.length} place{filteredDataByCity.length > 1 ? 's' : ''} to stay in Amsterdam</b>
             <PlacesSortForm />
             <div className="cities__places-list places__list tabs__content">
               {cardList}
