e288df7e5247a5504350bc953045d6bcd5b20f9b
IndieUnityMaker <ubivarik_serega@mail.ru>
2024-04-01 19:03:16 +0300

    add  state for currentSort

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,16 +1,18 @@
 import { createReducer } from '@reduxjs/toolkit';
-import { LOCATIONS, CitiesType } from '../constants';
+import { LOCATIONS, CitiesType, SortTypesType, SORT_TYPES } from '../constants';
 import { typeCard } from '../types.ts';
-import { changeCityAction, setOffersAction } from './actions.ts';
+import { changeCityAction, changeSortAction, setOffersAction } from './actions.ts';
 import { cardsData } from '../mock/card-data.ts';
 
 type InitialStateProps = {
   currentCity: CitiesType;
+  currentSort: SortTypesType;
   offers: typeCard[];
 };
 
 const initialState: InitialStateProps = {
   currentCity: LOCATIONS.Paris,
+  currentSort: SORT_TYPES.Popular,
   offers: cardsData,
 };
 
@@ -21,6 +23,9 @@ export const reducer = createReducer(initialState, (builder) => {
     })
     .addCase(setOffersAction, (state, action) => {
       state.offers = action.payload;
+    })
+    .addCase(changeSortAction, (state,action) => {
+      state.currentSort = action.payload;
     });
 });
 
